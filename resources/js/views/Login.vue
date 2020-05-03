be43d6fae4e969efacc3330dc30a6d82925b9046
Victor Nwaiwu <victor.nwaiwu@andela.com>
2020-05-03 15:39:03 +0100

    create App.vue and laravel controller as entry point for application

---
+++ resources/js/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Authentication/>
+  </div>
+</template>
+<script>
+import Authentication from '../components/Auth/Authentication';
+
+export default {
+  name: 'LoginComponent',
+  components: { Authentication },
+  props: {
+
+  },
+  created() {
+    console.log(' I am here');
+  }
+};
+</script>
