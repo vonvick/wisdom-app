2033b580877e9155b92d4da659eaef8eacb8459b
Victor Nwaiwu <victor.nwaiwu@andela.com>
2020-05-03 15:36:35 +0100

    remove laravel auth controllers, routes and view files

--- resources/views/admin.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layouts.app')
-
-@section('content')
-
-    <div id="admin">
-        <router-view></router-view>
-    </div>
-
-@endsection
--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layouts.app')
-
-@section('content')
-
-<div id="user">
-    <router-view></router-view>
-</div>
-
-@endsection
