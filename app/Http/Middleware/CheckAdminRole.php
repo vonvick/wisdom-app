c6b12b2afe1e300d5eaf4b70d66c8b2be5341ad9
Victor Nwaiwu <victor.nwaiwu@andela.com>
2020-05-10 22:56:17 +0100

    add middleware logic to CheckAdminRole.php

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\Role;
+use Exception;
 
 class CheckAdminRole
 {
@@ -15,6 +17,21 @@ class CheckAdminRole
      */
     public function handle($request, Closure $next)
     {
+        try {
+            $user_role = Role::find(auth()->user()->role_id);
+
+            if (!strpos($user_role->slug, 'admin')) {
+                throw new Exception('User not permitted to perform such action');
+            }
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => true,
+                'code' => 403,
+                'err' => $e->getMessage()
+            ], 403);
+        }
+
         return $next($request);
     }
 }
